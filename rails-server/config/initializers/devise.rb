ac0f554f5d9feadc6ff14843ce9eb4cf6abd2ec5
Emilio Ballardini <emilio.ballardini@aero.tur.ar>
2024-10-24 18:42:55 -0300

    removes token mehtod

--- rails-server/config/initializers/devise.rb
+++ rails-server/config/initializers/devise.rb
@@ -20,8 +20,6 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
-  config.token_authentication_key = 'access-token'
-
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
