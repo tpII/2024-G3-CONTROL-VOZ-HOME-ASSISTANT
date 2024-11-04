47c8cb1108627dbfa7cf15113f4a2accf0bc052f
Emilio Ballardini <emilioballardini@gmail.com>
2024-11-03 22:21:27 -0300

    Changes origins to 30002

--- rails-server/config/initializers/cors.rb
+++ rails-server/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080',
-            'http://127.0.0.1:8080'
+    origins 'http://localhost:3000',
+            'http://127.0.0.1:3000'
 
     resource '*',
              headers:     :any,
