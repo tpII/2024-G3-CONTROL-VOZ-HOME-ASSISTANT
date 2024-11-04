1744f62fbed51d39362aed194cc02e65d01c4328
Lautaro López <lautaro.lopez98@gmail.com>
2024-11-03 23:08:07 -0300

    Added auth and websockets connection

--- dashboard-pwa/src/app/(without-layout)/auth/login/page.tsx
+++ dashboard-pwa/src/app/(without-layout)/auth/login/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import { setCookie } from "cookies-next";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,9 +14,76 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Spinner } from "@/components/ui/spinner";
 import Link from "next/link";
+import { config } from "@/config";
+
+const { backendUrl } = config;
+const SIGN_IN_URL = `${backendUrl}/auth/sign_in`;
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isButtonDisabled, setButtonDisabled] = useState(true);
+  const [isLoading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const isValidEmail = validateEmail(email);
+    const isValidPassword = password.length > 0;
+    setButtonDisabled(!(isValidEmail && isValidPassword));
+    if (!isValidEmail && email.length > 0) {
+      setErrorMessage("Please enter a valid email address");
+    } else setErrorMessage("");
+  }, [email, password]);
+
+  const validateEmail = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setErrorMessage("");
+
+    try {
+      const response = await fetch(SIGN_IN_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const { data } = await response.json();
+        const {
+          access_token: { client, token },
+        } = data;
+        console.log({
+          token,
+          client,
+          email,
+        });
+        Cookies.set("token", token);
+        Cookies.set("client", client);
+        Cookies.set("uid", email);
+        router.push("/");
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message || "Login failed");
+      }
+    } catch (error) {
+      console.error("An error occurred", error);
+      setErrorMessage("An unexpected error occurred. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <section className="size-full flex justify-center items-center">
       <Card className="w-full max-w-md mx-auto bg-gray-900 border-gray-800">
@@ -21,35 +93,48 @@ export default function Login() {
             Enter your email and password to access your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              className="bg-gray-900 border-gray-800"
-              required
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              type="password"
-              className="bg-gray-900 border-gray-800"
-              required
-            />
-          </div>
-          <Link href="/auth/register" className="text-xs italic">
-            Don't have an account? Register
-          </Link>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full text-white bg-pink-500 hover:bg-pink-600">
-            Sign In
-          </Button>
-        </CardFooter>
+        <form onSubmit={handleSubmit}>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                className="bg-gray-900 border-gray-800"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                className="bg-gray-900 border-gray-800"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <Link href="/auth/register" className="text-xs italic">
+              Don't have an account? Register
+            </Link>
+            {errorMessage && (
+              <p className="text-red-500 text-sm">{errorMessage}</p>
+            )}
+          </CardContent>
+          <CardFooter>
+            <Button
+              className="w-full text-white bg-pink-500 hover:bg-pink-600"
+              type="submit"
+              disabled={isButtonDisabled || isLoading}
+            >
+              {isLoading ? <Spinner /> : "Sign In"}
+            </Button>
+          </CardFooter>
+        </form>
       </Card>
     </section>
   );
